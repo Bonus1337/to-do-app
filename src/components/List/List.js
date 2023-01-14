65f783087c999a4ab1a44bf97f7fb7993ddf2d28
Bonus1337 <10569@student.puzim.edu.pl>
2023-01-14 22:55:21 +0100

    Add react hooks

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,18 +1,44 @@
-import styles from './List.module.scss';
-import Column from '../Column/Column';
+import styles from "./List.module.scss";
+import Column from "../Column/Column";
+import { useState } from "react";
+import shortid from "shortid";
 
 const List = () => {
+  const [value, setValue] = useState("");
+  const [columns, setColumns] = useState([
+    { id: 1, title: "Books", icon: "book" },
+    { id: 2, title: "Movies", icon: "film" },
+    { id: 3, title: "Games", icon: "gamepad" },
+  ]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setColumns([...columns, { id: shortid(), title: value }]);
+    setValue("");
+  };
+
   return (
     <div className={styles.list}>
       <header className={styles.header}>
-        <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+        <h2 className={styles.title}>
+          Things to do<span>soon!</span>
+        </h2>
       </header>
-      <p className={styles.description}>Interesting thins I want to check out</p>
+      <p className={styles.description}>
+        Interesting thins I want to check out
+      </p>
       <section className={styles.columns}>
-        <Column title="Books" icon="book"/>
-        <Column title="Movies" icon="film"/>
-        <Column title="Games" icon="gamepad"/>
+        {columns.map((column) => (
+          <Column key={column.id} title={column.title} icon={column.icon} />
+        ))}
       </section>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button>Add column</button>
+      </form>
     </div>
   );
 };
