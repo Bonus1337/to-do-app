e02fff3de1e588ea4fcab4497b9c4c2de6aac542
Bonus1337 <10569@student.puzim.edu.pl>
2023-02-08 16:03:51 +0100

    Add Redux and Refactoring ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,24 +1,26 @@
-import styles from './ColumnForm.module.scss';
-import Button from '../Button/Button';
-import TextInput from '../TextInput/TextInput';
-import { useState } from 'react';
+import styles from "./ColumnForm.module.scss";
+import Button from "../Button/Button";
+import TextInput from "../TextInput/TextInput";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-
-
-const ColumnForm = props => {
-  const [title, setTitle] = useState('');
-  const [icon, setIcon] = useState('');
-  const handleSubmit = e => {
+const ColumnForm = (props) => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+  const [icon, setIcon] = useState("");
+  const handleSubmit = (e) => {
     e.preventDefault();
-    props.action({ title: title, icon: icon });
-    setTitle('');
-    setIcon('');
-  }
+    dispatch({ type: "ADD_COLUMN", newColumn: { title, icon } });
+    setTitle("");
+    setIcon("");
+  };
 
   return (
     <form className={styles.columnForm} onSubmit={handleSubmit}>
-      <span>Title:</span><TextInput value={title} onChange={e => setTitle(e.target.value)} />
-      <span>Icon:</span><TextInput value={icon} onChange={e => setIcon(e.target.value)}/>
+      <span>Title:</span>
+      <TextInput value={title} onChange={(e) => setTitle(e.target.value)} />
+      <span>Icon:</span>
+      <TextInput value={icon} onChange={(e) => setIcon(e.target.value)} />
       <Button>Add column</Button>
     </form>
   );
---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { createStore } from "redux";
+import initialState from "./initialState";
+
+const reducer = (state, action) => {
+  if (action.type === "ADD_COLUMN")
+    return { ...state, columns: [...state.columns, action.newColumn] };
+  return state;
+};
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
