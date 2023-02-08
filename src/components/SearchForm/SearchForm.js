7572ddb03db7b6474a0902c2f88b35a99267280a
Bonus1337 <10569@student.puzim.edu.pl>
2023-02-08 16:50:27 +0100

    Add creators and selectors

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,15 +3,16 @@ import TextInput from "../TextInput/TextInput";
 import Button from "../Button/Button";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { updateSearchString } from "../../redux/store";
 
 const SearchForm = () => {
   const dispatch = useDispatch();
   const [searchString, setSearchString] = useState("");
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: "UPDATE_SEARCHSTRING", payload: searchString });
+    dispatch(updateSearchString(searchString));
   };
+
   return (
     <form className={styles.searchForm} onSubmit={handleSubmit}>
       <TextInput
---
+++ src/utils/strContains.js
@@ -0,0 +1,3 @@
+export const strContains = (string1, string2) => {
+  return string1.toLowerCase().includes(string2.toLowerCase());
+};
